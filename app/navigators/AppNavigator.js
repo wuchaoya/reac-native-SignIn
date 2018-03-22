07d6323302da0d6f803f2539351557d6556ad558
wuchao <wuchao@haima.me>
2018-03-22 19:31:16 +0800

    集成icons

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, StackNavigator} from 'react-navigation';
+import { addNavigationHelpers, TabNavigator, TabBarBottom} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import LoginScreen from '../Components/LoginScreen';
 import MainScreen from '../Components/MainScreen';
 import ProfileScreen from '../Components/ProfileScreen';
 import { addListener } from '../util/redux';
 
-export const AppNavigator = StackNavigator({
-  Login: { screen: LoginScreen },
-  Main: { screen: MainScreen },
-  Profile: { screen: ProfileScreen }
-});
+export const AppNavigator = TabNavigator(
+  {
+    Login: { screen: LoginScreen },
+    Main: { screen: MainScreen },
+    Profile: { screen: ProfileScreen }
+  },
+	{
+		navigationOptions: ({ navigation }) => ({
+			tabBarIcon: ({ focused, tintColor }) => {
+				const { routeName } = navigation.state;
+				let iconName;
+				if (routeName === 'Home') {
+					iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+				} else if (routeName === 'Settings') {
+					iconName = `ios-options${focused ? '' : '-outline'}`;
+				}
+				
+				// You can return any component that you like here! We usually use an
+				// icon component from react-native-vector-icons
+				return <Ionicons name={iconName} size={25} color={tintColor} />;
+			},
+		}),
+		tabBarOptions: {
+			activeTintColor: 'tomato',
+			inactiveTintColor: 'gray',
+		},
+		tabBarComponent: TabBarBottom,
+		tabBarPosition: 'bottom',
+		animationEnabled: false,
+		swipeEnabled: false,
+	}
+);
 
 type Props = {
 	dispatch: PropTypes.func.isRequired,
---
+++ err.md
@@ -0,0 +1,5 @@
+react-native-vector-icons 
+
+react-native link react-native-vector-icons 后运行出错
+
+这个是系统版本引发的问题，因为 React Native 系统配置的文件和 react-native-vector-icons 里面的文件重复了，解决方法就是删除重复的文件，到 /node_modules/react-native/local-cli/core/_fixtures_/files/package.json 里面删除了 package.json 即可，删除之前记得备份一下文件。
